b7c486cd93fbcd340639f3d0a6b592f52811dac2
Jorge Zetien <jzetien@lsv-tech.com>
2024-08-12 17:59:29 -0500

    tomado otra fuente de videos a baja resolucion

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,54 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Un Bonito Paisaje..!',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Que borroso veo todo!',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Un Campo de flores',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': 'Atardecer por el retrovisor',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'Caminando la pasarela mientras veo el paisaje',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'las olas y el mar',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Puente sobre cascada',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': '¿Ya llegamos a la India?... umm si',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+
+
+];
+
+
